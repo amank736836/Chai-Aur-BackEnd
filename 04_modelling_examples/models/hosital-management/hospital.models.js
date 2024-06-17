fd23079311c494b0ce350e6231701784e256817b
Aman Kumar <amankarguwal0@gmail.com>
2024-06-17 23:24:36 +0530

    Create hospital.models.js

---
+++ 04_modelling_examples/models/hosital-management/hospital.models.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            // trim: true,
+        },
+        addressLine1: {
+            type: String,
+            required: true,
+        },
+        addressLine2: {
+            type: String,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        state: {
+            type: String,
+            required: true,
+        },
+        pincode: {
+            type: String,
+            required: true,
+        },
+        specializedIn: [
+            {
+                type: String
+            }
+        ],
+        
+
+    },
+    {timestamps: true}
+);
+
+const Hospital = mongoose.model("Hospital", hospitalSchema);
