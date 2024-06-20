ad5040e8b63ad8d0729a4e91d1b5af074f977f22
Aman Kumar <amankarguwal0@gmail.com>
2024-06-20 15:50:26 +0530

    middleware and cloudinary

---
+++ 05_Project(videoTube)/src/utils/cloudinary.js
@@ -0,0 +1,41 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath){
+            throw new Error("No File Path Provided");
+        }
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(
+            localFilePath,
+            {
+            resource_type: "auto",
+            // folder: "videoTube"
+            },
+            // (error, result) => {
+            //     if(error){
+            //         throw new Error(error);
+            //     }
+            //     return result;
+            // }
+        );
+        // file has been uploaded successfully on cloudinary
+        console.log("File is uploaded on cloudinary" , response.url);
+        console.log(response);
+        return response;
+    }
+    catch(error){
+        // console.error(error);
+        fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
