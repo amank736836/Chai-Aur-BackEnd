160b405304da0278b6f0ec81023567019a0d82f4
Aman Kumar <amankarguwal0@gmail.com>
2024-06-30 23:28:06 +0530

    chore: Update Vite config to proxy API requests to http://localhost:5000

--- 02_fullStack/frontEnd/vite.config.js
+++ 02_fullStack/frontEnd/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
+  index : {
     proxy : {
-      '/api' : 'http://localhost:4000'
+      '/api' : 'http://localhost:5000'
     }
   }
 })
