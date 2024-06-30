88628be7cc22fd5ee9df1950fc14530c480efdb3
Aman Kumar <amankarguwal0@gmail.com>
2024-07-01 00:02:02 +0530

    chore: Update express static path to include assets folder

--- 02_fullStack/backEnd/index.js
+++ 02_fullStack/backEnd/index.js
@@ -14,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the 'dist' directory
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('/', (req, res) => {
     res.send(`${__filename + ' ' + __dirname}`);
