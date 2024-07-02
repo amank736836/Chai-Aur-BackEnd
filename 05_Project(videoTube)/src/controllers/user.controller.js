85ad55f9004fe58806cca5b6859cd866574cf6f2
Aman Kumar <amankarguwal0@gmail.com>
2024-07-02 23:43:31 +0530

    chore: Import ApiResponse module in user.controller.js

--- 05_Project(videoTube)/src/controllers/user.controller.js
+++ 05_Project(videoTube)/src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
 import { deleteFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
-import { ApiResponse } from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
